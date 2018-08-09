b611a3f8c470d2098e66838c5fefeef984c2c640
Asylgareev Arthur <son.of.gallifrei@gmail.com>
2018-08-09 20:23:08 +0500

    Typos fixed

--- test/test_ecl_errors.c
+++ test/test_ecl_errors.c
@@ -13,6 +13,6 @@ void tearDown(void)
 void test_error_string( void )
 {
     TEST_ASSERT_MESSAGE(strcmp(error_string(OK), OK_STR) == 0, "OK error code was not handled correctly!");
-    TEST_ASSERT_MESSAGE(strcmp(error_string(FILE_NOT_FOUND), FILE_NOT_FOUND_STR) == 0, "FILE_NOT_FOUND_STR error code was not handled correctly!");
-    TEST_ASSERT_MESSAGE(strcmp(error_string(999), UNKNOWN_ERROR) == 0, "FILE_NOT_FOUND_STR error code was not handled correctly!");
+    TEST_ASSERT_MESSAGE(strcmp(error_string(FILE_NOT_FOUND), FILE_NOT_FOUND_STR) == 0, "FILE_NOT_FOUND error code was not handled correctly!");
+    TEST_ASSERT_MESSAGE(strcmp(error_string(999), UNKNOWN_ERROR) == 0, "UNKNOWN_ERROR error code was not handled correctly!");
 }
