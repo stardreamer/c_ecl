917e1ab8a4885fca7f455c680f82a274cee8fa63
Asylgareev Arthur <son.of.gallifrei@gmail.com>
2018-08-10 20:22:02 +0500

    Smoke test for get_file_descriptor was updated

--- test/test_nav.c
+++ test/test_nav.c
@@ -14,10 +14,11 @@ void tearDown(void)
 void test_smoke_nav( void )
 {
     char message[1024];
-    int error_code = get_file_descriptor(NULL, NULL);
+    int descriptor;
+    int error_code = get_file_descriptor("test/data/example.egrid", &descriptor);
 
     sprintf(message, "Smoke test for get_file_descriptor has failed! Expected: (%s) but was (%s)", error_string(OK), error_string(error_code));
-    TEST_ASSERT_MESSAGE(error_code == OK, message);
+    TEST_ASSERT_MESSAGE(error_code == OK && descriptor > 0, message);
 }
 
 void test_should_return_filenotfound_on_nonexisting_file()
