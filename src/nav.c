c1d795064f47c57e14b6edfd8e2a35abcb2ab3c0
Asylgareev Arthur <son.of.gallifrei@gmail.com>
2018-08-10 19:33:46 +0500

    Error handling for the case of nonexisting file was added

--- src/nav.c
+++ src/nav.c
@@ -1,7 +1,12 @@
 #include "nav.h"
-
+#include <unistd.h>
 
 int get_file_descriptor(char* filepath, int* descriptor)
 {
+    if(access(filepath, F_OK) == -1)
+    {
+        return FILE_NOT_FOUND;
+    }
+        
     return NOT_IMPLEMENTED;
 }
