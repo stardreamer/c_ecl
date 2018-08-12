5851668d87b32e1dd8345fe8218bfbea0dbe3c30
Asylgareev Arthur <son.of.gallifrei@gmail.com>
2018-08-12 17:51:21 +0500

    Function byte_reverse_in_place was optimized

--- src/utils.c
+++ src/utils.c
@@ -114,9 +114,53 @@ void byte_reverse_in_place(char *byte_stream, int len)
     {
         return;
     }
+
+
+    if(len == 4)
+    {
+        char tmp1 = byte_stream[0];
+        char tmp2 = byte_stream[1];
+        
+        byte_stream[0] = byte_stream[3];
+        byte_stream[3] = tmp1;
+        byte_stream[1] = byte_stream[2];
+        byte_stream[2] = tmp2;
+
+        return;
+    }
+
+    if(len == 8)
+    {
+        char tmp1 = byte_stream[0];
+        char tmp2 = byte_stream[1];
+        char tmp3 = byte_stream[2];
+        char tmp4 = byte_stream[3];
+
+        byte_stream[0] = byte_stream[7];
+        byte_stream[7] = tmp1;
+        byte_stream[1] = byte_stream[6];
+        byte_stream[6] = tmp2;
+        byte_stream[2] = byte_stream[5];
+        byte_stream[5] = tmp3;
+        byte_stream[3] = byte_stream[4];
+        byte_stream[4] = tmp4;
+
+        return;
+    }
     
     char *p1 = byte_stream;
     char *p2 = byte_stream + len - 1;
+
+    if(len == 4)
+    {
+        char tmp = *p1;
+        *p1++ = *p2;
+        *p2-- = tmp;
+        tmp = *p1;
+        *p1++ = *p2;
+        *p2-- = tmp;
+        return;
+    }
 
     while (p1 < p2) {
         char tmp = *p1;
